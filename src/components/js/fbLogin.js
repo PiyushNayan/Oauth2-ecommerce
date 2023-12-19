b7b3b5b5b6df3da2e3c6246f51b13bca02c3164c
Aditya Nair <adityanair@Adityas-MacBook-Pro.local>
2023-12-19 14:50:37 +0530

    logout button

--- src/components/js/fbLogin.js
+++ src/components/js/fbLogin.js
@@ -1,10 +1,13 @@
 import { defineComponent } from "vue";
 import { signInWithFacebook, logOut } from "@/firebase/facebooklogin.js";
+import AuthStore from "@/stores/authStore"
 
 export default defineComponent({
   setup() {
+    const authStore = AuthStore();
     const signIn = () => {
       signInWithFacebook();
+      authStore.updateAuthStatus("saAVDFBGNHJH")
     };
 
     const signOut = () => {
--- src/router/index.js
+++ src/router/index.js
@@ -1,58 +1,56 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import MyOrders from '../views/MyOrders.vue'
-import Mycart from '../views/MyCart.vue'
-import ProductList1 from '../components/ProductList1.vue'
-import ProductDetail from "../components/ProductDetail.vue"
+import { createRouter, createWebHistory } from "vue-router";
+import MyOrders from "../views/MyOrders.vue";
+import Mycart from "../views/MyCart.vue";
+import ProductList1 from "../components/ProductList1.vue";
+import ProductDetail from "../components/ProductDetail.vue";
 
-import LoginParent from "../components/LoginParent.vue"
-import about from '../views/AboutPage.vue'
-import OrderPage from "../components/OrderPage.vue"
+// import LoginParent from "../components/LoginParent.vue"
+import about from "../views/AboutPage.vue";
+import OrderPage from "../components/OrderPage.vue";
 
-import fbLogin from '../components/FbLogin.vue'
-
-
+import fbLogin from "../components/FbLogin.vue";
 
 const routes = [
   {
-    path: '/',
-    name: 'products',
-    component: ProductList1
+    path: "/",
+    name: "products",
+    component: ProductList1,
   },
   {
-    path: '/myorder',
-    name: 'order',
-    component: MyOrders
+    path: "/myorder",
+    name: "order",
+    component: MyOrders,
   },
   {
-    path: '/mycart',
-    name: 'cart',
-    component: Mycart
+    path: "/mycart",
+    name: "cart",
+    component: Mycart,
   },
   {
-    path: '/login',
-    name: 'login',
-    component: fbLogin
+    path: "/login",
+    name: "login",
+    component: fbLogin,
   },
   {
-    path: '/productDetail',
-    name: 'productDetail',
-    component: ProductDetail
+    path: "/productDetail",
+    name: "productDetail",
+    component: ProductDetail,
   },
   {
-    path: '/about',
-    name: 'about',
-    component: about
-  }, {
-    path: '/order',
-    name: 'order',
-    component: OrderPage
-  }
-
-]
+    path: "/about",
+    name: "about",
+    component: about,
+  },
+  {
+    path: "/order",
+    name: "order",
+    component: OrderPage,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
